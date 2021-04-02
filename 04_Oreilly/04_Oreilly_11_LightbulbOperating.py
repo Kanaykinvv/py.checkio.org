fd9dba29a0e0e24df459f48226c8a0d2fec71d2d
Виктор Канайкин <kanaykinvv@mail.ru>
2021-04-02 22:33:01 +0300

    add 04_Oreilly_11

--- 04_Oreilly/04_Oreilly_11_LightbulbOperating.py
+++ 04_Oreilly/04_Oreilly_11_LightbulbOperating.py
@@ -187,6 +187,10 @@ def sum_light(els: List[Union[datetime, Tuple[datetime, int]]],
         # Иначе False
         return False
 
+    # Функция сортировки массива
+    def sort_els(els_in: List[Union[datetime, Tuple[datetime, int]]]) -> List[Union[datetime, Tuple[datetime, int]]]:
+        pass
+
     # Проходим все временные отметки
     for index in range(len(els)):
 
