dd660173ff453af4e4b14ef114bf585ec20d577d
Виктор Канайкин <kanaykinvv@mail.ru>
2021-05-02 00:20:33 +0300

    add 04_Oreilly_18

--- 04_Oreilly/04_Oreilly_18_XsAndOsReferee.py
+++ 04_Oreilly/04_Oreilly_18_XsAndOsReferee.py
@@ -33,7 +33,13 @@
 from typing import List
 
 def checkio(game_result: List[str]) -> str:
-    return "D" or "X" or "O"
+    dashboard = [
+        [game_result[0][0], game_result[0][1], game_result[0][2]],
+        [game_result[1][0], game_result[1][1], game_result[1][2]],
+        [game_result[2][0], game_result[2][1], game_result[2][2]],
+        ]
+    print(dashboard)
+
 
 if __name__ == '__main__':
     print("Example:")
@@ -42,20 +48,20 @@ if __name__ == '__main__':
                    "XOO"]))
 
     #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio([
-        "X.O",
-        "XX.",
-        "XOO"]) == "X", "Xs wins"
-    assert checkio([
-        "OO.",
-        "XOX",
-        "XOX"]) == "O", "Os wins"
-    assert checkio([
-        "OOX",
-        "XXO",
-        "OXX"]) == "D", "Draw"
-    assert checkio([
-        "O.X",
-        "XX.",
-        "XOO"]) == "X", "Xs wins again"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+    # assert checkio([
+    #     "X.O",
+    #     "XX.",
+    #     "XOO"]) == "X", "Xs wins"
+    # assert checkio([
+    #     "OO.",
+    #     "XOX",
+    #     "XOX"]) == "O", "Os wins"
+    # assert checkio([
+    #     "OOX",
+    #     "XXO",
+    #     "OXX"]) == "D", "Draw"
+    # assert checkio([
+    #     "O.X",
+    #     "XX.",
+    #     "XOO"]) == "X", "Xs wins again"
+    # print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
