6915cbb12e5d8e5e67aae4456d74c1ddc71903a8
Виктор Канайкин <kanaykinvv@mail.ru>
2021-05-04 22:51:19 +0300

    add 05_GitHub_02

--- 05_GitHub/05_GitHub_02_TheCheapestFlight.py
+++ 05_GitHub/05_GitHub_02_TheCheapestFlight.py
@@ -30,8 +30,53 @@ from typing import List
 
 
 def cheapest_flight(costs: List, a: str, b: str) -> int:
-    # your code here
-    return None
+    # "Алгоритм Дейкстры"
+    # https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%94%D0%B5%D0%B9%D0%BA%D1%81%D1%82%D1%80%D1%8B
+
+    # Начальная вершина имеет вес равный 0
+    weights = {a: 0}
+
+    # Остальные вершины максимальный вес
+    for cost in costs:
+        if cost[0] not in weights.keys():
+            weights[cost[0]] = int(2**32)
+        if cost[1] not in weights.keys():
+            weights[cost[1]] = int(2**32)
+
+    # Множество посещенных вершин (на старте пустое)
+    visitedPeaks = set()
+
+    # Нахождение вершины с минимальным весом
+    def search_min_unvisited_peaks() -> str:
+        """
+        Нахождение вершины с минимальным весом
+        :return: Имя вершины с минимальным весом
+        """
+        current_element = ""
+        current_weight = 2**32
+        for key in weights.keys():
+            if (key not in visitedPeaks) and (weights[key] <= current_weight):
+                current_element = key
+                current_weight = weights[key]
+        return current_element
+
+    while len(visitedPeaks) != len(weights.keys()):
+        element = search_min_unvisited_peaks()
+
+        # Проходим все входные списки
+        for cost in costs:
+            # Находим стартовую текущую точку
+            if cost[0] == element:
+                # Находим наименьший вес до второй точки
+                if cost[2] + weights[element] <= weights[cost[1]]:
+                    weights[cost[1]] = cost[2] + weights[element]
+
+        # Отмечаем вершину как пройденную
+        visitedPeaks.add(element)
+
+    return weights[b]
+
+
 
 
 if __name__ == '__main__':
