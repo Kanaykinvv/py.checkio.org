ffddf9f98f409315265df050f2894187b2e488a5
Виктор Канайкин <kanaykinvv@mail.ru>
2021-05-27 23:21:53 +0300

    add 06_Dropbox_02

--- 06_Dropbox/06_Dropbox_02_TheBestNumberEver.py
+++ 06_Dropbox/06_Dropbox_02_TheBestNumberEver.py
@@ -19,15 +19,40 @@ import random
 
 
 def checkio():
-    x = random.randint(1000, 9999)
-    list_int = list(char for char in str(x))
-    maxi = "".join([str(item) for item in sorted(list_int, reverse=True)])
-    mini = "".join([str(item) for item in sorted(list_int, reverse=False)])
+    """
+    Операция Капрекара.
+    В 1949 году математик Д. Р. Капрекар из Деолали, Индия, разработал процесс, известный теперь как операция Капрекара.
+    Сначала выберем четырехзначное число, состоящее хотя бы из двух различных цифр. Затем переставим его цифры, чтобы
+    получить самое большое и самое маленькое из возможных чисел, образованных цифрами этого числа. Наконец, вычтем самое
+    маленькое число из самого большого, получим новое число, для которого снова повторим операцию.
+    Когда мы получим число 6174, операция повторяется, каждый раз давая 6174.
+    :return: 6174
+    """
+    result = random.randint(1001, 9988)
+    iteration = 0
+    show_hint = True
 
-    print(maxi)
-    print(mini)
+    def minus_maxi_mini(x: int) -> int:
+        list_int = list(char for char in str(x))
+        maxi = "".join([str(item) for item in sorted(list_int, reverse=True)])
+        mini = "".join([str(item) for item in sorted(list_int, reverse=False)])
+        if show_hint: print("enter integer = " + str(x))
+        if show_hint: print("maxi = " + str(maxi))
+        if show_hint: print("mini = " + str(mini))
+        if show_hint: print("outer integer = " + str(int(maxi) - int(mini)))
+        return int(maxi) - int(mini)
 
-    return 73  # if you are Sheldon
+    while result != 6174:
+        result = minus_maxi_mini(result)
+        iteration += 1
+        if show_hint: print("iteration = " + str(iteration))
+        if show_hint: print("-"*50)
+        if iteration > 10:
+            break
+
+    if show_hint: print("final iteration = " + str(iteration))
+
+    return 6174
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
